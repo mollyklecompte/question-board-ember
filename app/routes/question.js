02dd0668844fa8c1c3a9e91f069aa555adc940e6
molly lecompte <mollyklecompte@gmail.com>
2016-10-28 16:09:30 -0700

    add question-detail component without content.

--- app/routes/question.js
+++ app/routes/question.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model(params) {
+  return this.store.findRecord('question', params.question_id);
+  },
+
+  actions: {
+    destroyQuestion(question) {
+      rental.destroyRecord();
+      this.transitionTo('index');
+    }
+  }
 });
