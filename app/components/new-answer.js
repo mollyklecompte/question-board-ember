74863c49d4c50d42e4c3762ae33e6f47f5057fe3
molly lecompte <mollyklecompte@gmail.com>
2016-10-28 17:36:25 -0700

    add answer component.

---
+++ app/components/new-answer.js
@@ -0,0 +1,20 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  addNewAnswer: false,
+  actions: {
+    answerFormShow() {
+      this.set('addNewAnswer', true);
+    },
+    saveAnswer() {
+     var params = {
+       author: this.get('author'),
+       rating: this.get('rating'),
+       content: this.get('content'),
+       question: this.get('question')
+     };
+     this.set('addNewAnswer', false);
+     this.sendAction('saveAnswer', params);
+   }
+  }
+});
