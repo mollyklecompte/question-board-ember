baa36f8c0aa00674b72b14815a150427bb3191d2
molly lecompte <mollyklecopte@gmail.com>
2016-11-04 08:25:07 -0700

    add computed property to display the number of answers each question has on its question-tile template.

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  // answerCount: Ember.computed('question.answers', function() {
-  //   return Object.keys(this.get('question.answers')).length;
-  // }),
+  answerCount: Ember.computed('question.answers', function() {
+    return this.get('question.answers').get('length');
+  }),
 });
