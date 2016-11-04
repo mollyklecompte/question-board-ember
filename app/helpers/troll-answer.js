1b1d61d1abf764e852fb11d00d61726232573e2f
molly lecompte <mollyklecopte@gmail.com>
2016-11-04 10:01:45 -0700

    add helper to render troll icon next to answers with less than -3 votes.

--- app/helpers/troll-answer.js
+++ app/helpers/troll-answer.js
@@ -1,7 +1,11 @@
 import Ember from 'ember';
 
 export function trollAnswer(params/*, hash*/) {
-  return params;
+  var answerVotes = parseInt(params[0].get('votes'));
+
+  if (answerVotes < -3) {
+    return Ember.String.htmlSafe('<span class="troll-icon"><img src="https://cdn2.iconfinder.com/data/icons/identificon/96/troll-512.png"><span>');
+  }
 }
 
 export default Ember.Helper.helper(trollAnswer);
