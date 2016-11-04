94d72ddd7a47d520dcf69059d33a29f67565edea
molly lecompte <mollyklecopte@gmail.com>
2016-11-04 08:57:37 -0700

    add custom helper to display glyphicon next to questions with at least 5 answers.

---
+++ tests/unit/helpers/most-upvotes-test.js
@@ -0,0 +1,10 @@
+import { mostUpvotes } from 'question-board-ember/helpers/most-upvotes';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | most upvotes');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = mostUpvotes([42]);
+  assert.ok(result);
+});
